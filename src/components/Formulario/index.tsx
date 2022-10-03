71c9bf21d1fe55e0864f70d52bd6ef94be01991a
Michel Marinho <michel.marinho04@outlook.com>
2022-10-03 11:39:16 -0400

    Props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -27,7 +27,9 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <Botao />
+        <Botao>
+            Adicionar
+        </Botao>
       </form>
     );
   }
